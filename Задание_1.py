4108c2f670fed6819a12645ca6b8b17eaf314b7f
Анна Шинева <anna.smol@mail.ru>
2023-02-12 23:02:47 +0300

    solved

--- Задание_1.py
+++ Задание_1.py
@@ -1,20 +1,22 @@
 import requests
 
-with open('superhero_list', 'rb') as f:
-    response = requests.put(url='https://akabab.github.io/superhero-api/api', f)
-    response.raise_for_status()
-
 
 def most_intel_superhero(superheros_name):
-    superheros_intelligence = {}
-    intelligence = []
-    url = 'https://akabab.github.io/superhero-api/api/all.json'
-    response = requests.get(url=url)
-    superheros_list = superheros_name.split(', ')
-    for superheros_name in response:
-        intelligence = max(intelligence)
-    print (f'Самый сильный супергерой - {superheros_name}')
+    url = 'https://akabab.github.io/superhero-api/api/all.json' #задаем переменную для url
+    response = requests.get(url=url) #получаем данные со страницы
+    superheros_list = superheros_name.split(', ') #создаем переменную для списка с именами, которые задаем при перечислении супергероев
+    superheros_intelligence = {} #создаем словарь, куда поместим имя и интеллект супергероя
+    intelligence = [] #создаем список для значений интеллекта
+    for i in response.json():
+        if i.get('name') in superheros_list: #получаем имя супергероя и проверяем, есть ли оно в списке супергероев, который мы задали
+            superheros_intelligence[i['name']] = i['powerstats']['intelligence'] #заполняем словарь именем и интеллектом супергероя
+    for number in superheros_intelligence.values():
+        intelligence.append(int(number)) #пополняем список со значениями интеллекта
+    for key, value in superheros_intelligence.items(): #создаем цикл для сравнения максимального значения интеллека по списку со значениями словаря и выводим имя
+        if value == max(intelligence):
+            res = f'Самый умный супергерой - {key}'
+    return print(res)
 
 
 if __name__ == '__main__':
-    most_intel_superhero('Hulk', 'Captain America', 'Thanos')
+    most_intel_superhero('Captain America, Thanos, Hulk')
--- Задание_2.py
+++ Задание_2.py
@@ -21,33 +21,17 @@ class YaUploader:
         return response.json()
 
     def upload_file_to_disk(self, path_to_file, file_name):
-        href = self._get_upload_link(path_to_file=path_to_file).get("href", "")
-        with open('file_name', 'rb') as file:
+        href = self._get_upload_link(path_to_file=path_to_file).get('href', '')
+        with open(file_name, 'rb') as file:
             response = requests.put(href, file)
-            response.raise_for_status()
+            response.raise_for_status
             if response.status_code == 201:
                 print("Success")
-                return
 
 
 if __name__ == '__main__':
-    path_to_file = '/Users/annasmolina/PycharmProjects/pythonProject/HW/HW3/Mscw.jpg'
+    path_to_file = '/Mscw.jpg'
     token = ''
     file_name = 'Mscw.jpg'
     uploader = YaUploader(token=token)
     result = uploader.upload_file_to_disk(path_to_file, file_name)
-
-
-
-    # def upload_file(self, loadfile, savefile):
-    #     savefile = self.get("https://cloud-api.yandex.net/v1/disk/resources", 'rb')
-    #     loadfile = "HW/HW2 - открытие,чтение,запись файла/recipes.txt"
-    #     response = requests.put(savefile, data=open(loadfile, 'rb'))
-    #     response.raise_for_status()
-    #     if response.status_code == 201:
-    #         print("Success")
-    #
-    # if __name__ == '__main__':
-    #     token = ''
-    #     uploader = YaUploader(token)
-    #     print(uploader)
